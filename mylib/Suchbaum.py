bcc86b7cc78559c005bdcf7293fffcd7750c1203
Muetzilla <muetz-github@gasbichler.ch>
2025-11-28 15:19:50 +0100

    guete

--- mylib/Suchbaum.py
+++ mylib/Suchbaum.py
@@ -1,4 +1,8 @@
 from graphviz import Digraph
+from networkx.classes import number_of_nodes
+
+from mylib.Baumknoten import Baumknoten
+
 
 class Suchbaum:
     def __init__(self, anker):
@@ -14,6 +18,41 @@ class Suchbaum:
             else:
                 current = current.right_child
         return None
+
+    def insertNode(self, node):
+        current = self.anker
+        parent = None
+        isInsertionDone = False
+        if self.anker is None:
+            self.anker = node
+            isInsertionDone = True
+        while not isInsertionDone:
+            parent = current
+            if node.key == current.key:
+                raise KeyError("Schlüssel bereits vorhanden, bitte nur eindeutige Schlüssel verwenden.")
+            elif node.key < current.key:
+                if current.left_child is None:
+                    isInsertionDone = True
+                else:
+                    current = current.left_child
+            else:
+                if current.right_child is None:
+                    isInsertionDone = True
+                else:
+                    current = current.right_child
+        node.parent = parent
+        if node.key < parent.key:
+            parent.left_child = node
+        else:
+            parent.right_child = node
+
+    def insert(self, schluessel, name, daten):
+        neuer_knoten = Baumknoten(schluessel, name, daten, None, None, None)
+        if self.anker is None:
+            self.anker = neuer_knoten
+        else:
+            self.insertNode(neuer_knoten)
+
 
     def einfuegen(self, neuer_knoten):
         current = self.anker
@@ -32,6 +71,15 @@ class Suchbaum:
         else:
             parent.left_child = neuer_knoten
         neuer_knoten.parent = parent
+
+    def delete(self, knoten):
+        def is_leaf(node):
+            return node.left_child is None and node.right_child is None
+        if is_leaf(knoten):
+            if knoten.parent.left_child == knoten:
+                knoten.parent.left_child = None
+            else:
+                knoten.parent.right_child = None
 
     def __str__(self):
         anzeige = "-"
@@ -82,13 +130,30 @@ class Suchbaum:
         def add_nodes_edges(node):
             if node.left_child is not None:
                 dot.node(str(node.left_child.key))
-                dot.edge(str(node.key), str(node.left_child.key))
+                dot.edge(str(node.key), str(node.left_child.key), color="red")
                 add_nodes_edges(node.left_child)
             if node.right_child is not None:
                 dot.node(str(node.right_child.key))
-                dot.edge(str(node.key), str(node.right_child.key))
+                dot.edge(str(node.key), str(node.right_child.key), color="green")
                 add_nodes_edges(node.right_child)
 
         add_nodes_edges(self.anker)
         dot.render('binary_tree', view=True, format='png')
 
+    def guete(self):
+        if self.anker is None:
+            return 0
+        guete_sum = 0
+        guete_number_of_nodes = 0
+        def depth(node, guete_depth):
+            nonlocal guete_sum, guete_number_of_nodes
+            if node is not None:
+                depth(node.left_child, guete_depth + 1)
+                depth(node.right_child, guete_depth + 1)
+                guete_sum += guete_depth + 1
+                guete_number_of_nodes += 1
+
+        depth(self.anker, 0)
+        print("Summe der Tiefen:", guete_sum)
+        print("Anzahl der Knoten:", guete_number_of_nodes)
+        return guete_sum / guete_number_of_nodes if guete_number_of_nodes > 0 else 0
