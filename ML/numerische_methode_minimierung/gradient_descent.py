4acec971e3d41614ef6106c573bf1498e219ca87
Muetzilla <muetz-github@gasbichler.ch>
2026-04-08 07:59:09 +0200

    Gradient Descent

--- ML/numerische_methode_minimierung/gradient_descent.py
+++ ML/numerische_methode_minimierung/gradient_descent.py
@@ -1,10 +1,16 @@
 # Gradient Descend für f(x,y) = (x-2)^2 + 10(y-4)^2 -1
+from ML.lib  import *
+from ML.lib.visualize import plot_function
+
 
 def calc_part_derivative_x(x, y):
     return 2 * (x - 2)
 
 def calc_part_derivative_y(x, y):
     return 20 * (y - 4)
+
+def grad_f(x, y):
+    return 2 * (x - 2), 20 * (y - 4)
 
 def calc_function(x, y):
     return (x - 2) ** 2 + 10 * (y - 4) ** 2 - 1
@@ -27,3 +33,5 @@ for i in range(n):
 
 print(f"\nMinimum bei: x = {x_new:.4f} y = {y_new:.4f}")
 print(f"\nFunktionswert an Minimum:{ calc_function(x_new, y_new):.6f}")
+
+plot_function(calc_function,start_point=(x_start, y_start), grad=grad_f(x_start, y_start), x_range=(-10, 10), y_range=(-10, 10))
---
+++ ML/numerische_methode_minimierung/gradient_descent_num_der.py
@@ -0,0 +1,29 @@
+# Gradient Descend für f(x,y) = (x-2)^2 + 10(y-4)^2 -1 mit Numerischer Ableitung
+from ML.lib  import *
+from ML.lib.visualize import plot_function
+
+
+def grad_numeric(x, y, h=1e-5):
+    dfdx = (calc_function(x + h, y) - calc_function(x - h, y)) / h
+    dfdy = (calc_function(x + h, y) - calc_function(x - h, y)) / h
+    return dfdx, dfdy
+
+def calc_function(x, y):
+    return (x - 2) ** 2 + 10 * (y - 4) ** 2 - 1
+
+alpha = 0.01
+n = 500
+x_start = 8
+y_start = -3
+x_new = x_start
+y_new = y_start
+
+for i in range(n):
+    x_old = x_new
+    y_old = y_new
+    x_new, y_new = grad_numeric(x_old, y_old)
+    print(f"Iteration {i + 1}: x={x_new:.4f}, y={y_new:.4f}")
+
+print(f"\nMinimum bei: x = {x_new:.4f} y = {y_new:.4f}")
+print(f"\nFunktionswert an Minimum:{ calc_function(x_new, y_new):.6f}")
+
